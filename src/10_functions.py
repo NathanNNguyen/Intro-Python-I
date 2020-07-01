2dedc3ba80f42813a8fb298c5ac786e905c97ad7
Nhan <natenguyen51294@gmail.com>
2020-06-30 19:00:46 -0700

    added function

--- src/10_functions.py
+++ src/10_functions.py
@@ -10,3 +10,13 @@ num = int(num)
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    if(num % 2 == 0):
+        print('Even!')
+        return True
+    else:
+        print('Odd')
+
+
+z = is_even(num)
